0aaf501c8a173231233ab6fe1245e4bdb0f4c1f0
osamh112 <130056119+osamh112@users.noreply.github.com>
2023-08-13 02:18:21 +0300

    Add files via upload

---
+++ osamh112.py
@@ -0,0 +1,153 @@
+from datetime import *
+
+def dan(y=0,m=0,d=0):
+ date = datetime.now()
+ if len(list(str(y))) == 4:
+  if m <= 12 and m > 0:
+   if d <= 31 and d > 0:
+    if date.year <= y:
+     if date.month <= m:
+      if date.day < d:
+       return True
+x = dan(2025,9,25)
+     
+if x:pass
+else:quit(' تم ايقاف الاداه اذهب للجلاد حاكم الهكسوس ليقوم بتشغيلها!!!')
+print(" The tools is working go to osamh now if you need update this tools")
+import webbrowser
+import os
+
+#@PY_A0
+
+os.system('pip install webbrowser')
+import os
+os.system('pip install os')
+
+
+print(' \n        WELCOME TO OSAMH TOOLS ')
+
+
+webbrowser.open('https://t.me/HACKEEM_AL_HACKSOOS')
+
+#كسمك اذا فتحت التشفير عوافي
+
+
+
+
+import random
+import time, requests, webbrowser
+from time import sleep
+import os
+import sys
+import pyfiglet
+from pyfiglet import figlet_format
+A = "\033[1;91m"
+B = "\033[1;90m"
+C = "\033[1;97m"
+E = "\033[1;92m"
+H = "\033[1;93m"
+K = "\033[1;94m"
+L = "\033[1;95m"
+
+from rich.panel import Panel as Ch
+from rich import print as code
+code(Ch('                         THE KING OF HACKSOSS )) 
+
+import os
+s = '\033[1;36m'
+a = '\033[1;33m'
+z = '\033[1;31m'
+
+try:
+ from cfonts import render, say
+except:
+ os.system('pip install python-cfonts')
+din = render(f'osamh', colors=['red', 'yellow'], align='center')
+print(din)
+print(s+'*'*60)
+
+logo1 = figlet_format('KY_112',font ='banner3')
+print(H+logo1)
+print(E+'- '*25)
+token = input(H+'- Enter the token bot :  ')
+ID = input(K+'-Enter the ID :  ')
+os.system('clear')
+logo2 = figlet_format('osamh',font ='banner3')
+print(logo2)
+print(H+'∞'*30)
+print("[1] USER #_#_#\n\n[2] USER ##_##\n\n[3] USER ###BOT ")
+print(H+'∞'*30)
+H_P_O= input('Enter the number if you need hack users:  ')
+print(H+'∞'*30)
+def sh3():
+	os.system('clear')
+	logo1 = figlet_format('osamh #_#_#',font ='banner3-D')
+	print(logo1)
+	print(H+'_'*60)
+	while True:
+		oip1 ='QWERTYUIOPLKMNJHBVGFCXDSZA'
+		all1 = 'QAZXSWEDCVFRTGBNHYUJMKIOLP1234567890'
+		u1 = str(''.join(random.choice(oip1)for i in range(1)))
+		u2 = str(''.join(random.choice(all1)for i in range(1)))
+		u3 = str(''.join(random.choice(all1)for i in range(1)))
+		tr = '_'
+		user11 = u1+tr+u2+tr+u3
+		url = f"https://t.me/{user11}"
+		req = requests.get(url)
+		if req.text.find('If you have <strong>Telegram</strong>, you can contact <a class="tgme_username_link"')>=0: 
+			print(f" \033[1;32m - work >> [ {user11} ]")
+			req = requests.post(f'''https://api.telegram.org/bot{token}/sendMessage?chat_id={ID}&text=- سجل دخول الجلاد حاكم الهكسوس شخصيا
+𝐓𝐇𝐄 𝐊𝐈𝐍𝐆 𝐎𝐅 𝐇𝐀𝐂𝐊𝐒𝐎𝐒𝐒 .\n. — —— — —  — — — — — .\n- اليوزر يافخمي :  @{user11} \n 
+by : @KY_112:يوزر المطور الجلاد -  @HACKEEM_AL_HACKSOOS :رابط قناه الجلاد ''')
+		else:
+			print(f"\033[1;31m - Not Work >> [ {user11} ]")
+def sh4():
+	os.system('clear')
+	logo1 = figlet_format('osamh ##_##',font ='banner3-D')
+	print(logo1)
+	print(H+'_'*60)
+	while True:
+		oip2 ='QWERTYUIOPLKMNJHBVGFCXDSZA'
+		all2 = 'QAZXSWEDCVFRTGBNHYUJMKIOLP1234567890'
+		u1 = str(''.join(random.choice(oip2)for i in range(1)))
+		u2 = str(''.join(random.choice(all2)for i in range(1)))
+		u3 = str(''.join(random.choice(all2)for i in range(1)))
+		u4 = str(''.join(random.choice(all2)for i in range(1)))
+		tr ='_'
+		user22 = u1+u2+tr+u3+u4
+		url = f"https://t.me/{user22}"
+		req = requests.get(url)
+		if req.text.find('If you have <strong>Telegram</strong>, you can contact <a class="tgme_username_link"')>=0: 
+			print(f" \033[1;32m - work >> [ {user22} ]")
+			req = requests.post(f'''https://api.telegram.org/bot{token}/sendMessage?chat_id={ID}&text=- سجل دخول الجلاد حاكم الهكسوس شخصيا 
+𝐓𝐇𝐄 𝐊𝐈𝐍𝐆 𝐎𝐅 𝐇𝐀𝐂𝐊𝐒𝐎𝐒𝐒 .\n. — —— — —  — — — — — .\n- اليوزر يافخمي :  @{user22} \n 
+by : @KY_112:يوزر المطور الجلاد -  @HACKEEM_AL_HACKSOOS :رابط قناه الجلاد ''')
+		else:
+			  print(f"\033[1;31m - Not Work >> [ {user22} ]")
+def bot3():
+	os.system('clear')
+	logo1 = figlet_format('osamh ###BOT',font ='banner3-D')
+	print(logo1)
+	print(H+'_'*60)
+	while True:
+		oip3='QWERTYUIOPLKMNJHBVGFCXDSZA'
+		all3 = 'QAZXSWEDCVFRTGBNHYUJMKIOLP1234567890'
+		u1 = str(''.join(random.choice(oip3)for i in range(1)))
+		u2 = str(''.join(random.choice(all3)for i in range(1)))
+		u3 = str(''.join(random.choice(all3)for i in range(1)))
+		user33 = u1+u2+u3+'BOT'
+		url = f"https://t.me/{user33}"
+		req = requests.get(url)
+		if req.text.find('If you have <strong>Telegram</strong>, you can contact <a class="tgme_username_link"')>=0: 
+			print(f" \033[1;32m - work >> [ {user33} ]")
+			req = requests.post(f'''https://api.telegram.org/bot{token}/sendMessage?chat_id={ID}&text=- سجل دخول الجلاد حاكم الهكسوس شخصيا
+𝐓𝐇𝐄 𝐊𝐈𝐍𝐆 𝐎𝐅 𝐇𝐀𝐂𝐊𝐒𝐎𝐒𝐒 .\n. — —— — —  — — — — — .\n- اليوزر يافخمي :  @{user33} \n 
+by : @KY_112:يوزر المطور الجلاد -  @HACKEEM_AL_HACKSOOS :رابط قناه الجلاد ''')
+		else:
+			  print(f"\033[1;31m - Not Work >> [ {user33} ]")
+if H_P_O=='1':
+	sh3()
+if H_P_O=='2':
+	sh4()
+if H_P_O=='3':
+	bot3()
